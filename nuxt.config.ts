f174f3e7a54c59feb1ff25595f058f8cebe7ca67
Firstname Lastname <your_email@youremail.com>
2022-10-10 17:48:34 +0100

    push

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,8 @@ import { defineNuxtConfig } from 'nuxt';
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/color-mode'],
-
+  target: 'static',
+  ssr: false,
   tailwindcss: {
     cssPath: '~/assets/css/tailwind.css',
     configPath: 'tailwind.config.js',
